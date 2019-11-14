bf2c5949c2b2c2913cd8d65df35a1bdf694747f2
Ryan Vint <rfv501@york.ac.uk>
2019-11-14 13:55:50 +0000

    Patrol partially implemented
    Still needs patrolPath attribute added
    patrol method set up

--- src/Patrol.java
+++ src/Patrol.java
@@ -1,4 +1,19 @@
 
-public class Patrol{
+public class Patrol extends Unit{
 
+	//patrolPath attribute needs to be added here and then put into the constructor
+	
+	//Constructor
+	public Patrol(int currentHealth, int maxHealth, int range, int damage, int currentLocationX, int currentLocationY,
+			int moveLocationX, int moveLocationY, int speed) {
+		super(currentHealth, maxHealth, range, damage, currentLocationX, currentLocationY, moveLocationX, moveLocationY, speed);
+		
+	}
+
+	//patrolPath Getter and Setter here
+	
+	//Methods
+	public void patrol() {
+		
+	}
 }
