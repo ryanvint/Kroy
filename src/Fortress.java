ea58110a98e244d01ad95c4a31e7b719f72d796d
Ryan Vint <rfv501@york.ac.uk>
2019-11-14 14:08:01 +0000

    Fortress set up

--- src/Fortress.java
+++ src/Fortress.java
@@ -1,4 +1,26 @@
 
-public class Fortress {
+public class Fortress extends Entity{
+
+	public boolean hasBoss;
+	
+	public Fortress(int currentHealth, int maxHealth, int range, int damage, int currentLocationX,
+			int currentLocationY, boolean hasBoss) {
+		super(currentHealth, maxHealth, range, damage, currentLocationX, currentLocationY);
+		this.hasBoss = hasBoss;
+	}
+
+	//Getters and setters
+	public boolean isHasBoss() {
+		return hasBoss;
+	}
+
+	public void setHasBoss(boolean hasBoss) {
+		this.hasBoss = hasBoss;
+	}
+	
+	//Methods
+	public void spawnPatrol() {
+		
+	}
 
 }
