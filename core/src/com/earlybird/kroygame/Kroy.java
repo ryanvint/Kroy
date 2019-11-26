1b0fd7ce3a20aa588eefc4ce9f4f673270660a52
jameslittlecs <56447157+jameslittlecs@users.noreply.github.com>
2019-11-26 17:03:17 +0000

    Fixed Asset Build

--- core/src/com/earlybird/kroygame/Kroy.java
+++ core/src/com/earlybird/kroygame/Kroy.java
@@ -27,8 +27,6 @@ public class Kroy extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		if Gdx.input.isKeyPressed(UP);
-		
 		batch.begin();
 		batch.draw(img, 1, 1);
 		batch.end();
