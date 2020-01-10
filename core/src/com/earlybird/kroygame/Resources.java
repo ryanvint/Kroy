2b5c9a438059cbfb01d9ffbe38f135b4068fb321
jameslittlecs <56447157+jameslittlecs@users.noreply.github.com>
2020-01-10 21:06:46 +0000

    Revert "Update Resources.java"
    This reverts commit b8aed8a6e8e01e41113fc97fbc36920a42316375.

--- core/src/com/earlybird/kroygame/Resources.java
+++ core/src/com/earlybird/kroygame/Resources.java
@@ -1,19 +1,19 @@
 package com.earlybird.kroygame;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Resources {
 	
-	//Class works by assigning variables shown below with texture found within Texture Atlas's found within Desktop/packed .findregion finds information under name within "" 
-	//this then shows what texture to assign from the PNG file linked to the info
-	
 	TextureAtlas gameSprites;
 	TextureAtlas gameLocations;
+	TextureAtlas;
 	
-	public TextureRegion ground; //Initiates "Texture Regions" which are objects that are assigned textures later on within MainGameScreen
-	public TextureRegion road;	
+	public TextureRegion ground;
+	public TextureRegion road;
 	public TextureRegion firetruck;
 	public TextureRegion fortress1;
 	public TextureRegion fortress2;
@@ -22,15 +22,14 @@ public class Resources {
 	
 	public Resources()
 	{
-		gameLocations = new TextureAtlas(Gdx.files.internal("packed/gameLocations.txt"));
-		gameSprites = new TextureAtlas(Gdx.files.internal("packed/game.atlas")); //Shows what texture atlas to use for the gameSprite atlas and
-		//links to the PNG file, where textures are stored
+		//gameLocations = new TextureAtlas(Gdx.files.internal("packed/gameLocation.atlas"));
+		gameSprites = new TextureAtlas(Gdx.files.internal("packed/game.atlas"));
 		
-		ground = gameSprites.findRegion("ground"); //Assigns ground variable with a Texture
-		road = gameSprites.findRegion("road"); //Assigns road variable with a Texture
-		firetruck = gameSprites.findRegion("firetruck"); //Assigns firetruck variable with a Texture from the Atlas. Item in "" links to sheet which then pulls item from PNG
-		//fortress1 = gameLocations.findRegion("Fortress"); //Assigns fortress1 variable with a Texture
-		//firestation = gameLocations.findRegion("Firestation"); //Assigns firestation variable with a Texture
+		ground = gameSprites.findRegion("ground");
+		road = gameSprites.findRegion("road");
+		firetruck = gameSprites.findRegion("firetruck");
+		fortress1 = gameSprites.findRegion("firetruck");
+		firestation = gameSprites.findRegion("firetruck");
 	}
 	
 	public void dispose()
@@ -38,4 +37,3 @@ public class Resources {
 		gameSprites.dispose();
 	}
 }
- 
