7d2bec568ba655e0d4682a77173e39327e4ce0f7
ryanvint <rfv501@york.ac.uk>
2020-01-18 18:14:58 +0000

    Commentsssss

--- core/src/com/earlybird/kroygame/Engines.java
+++ core/src/com/earlybird/kroygame/Engines.java
@@ -4,21 +4,32 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 
+/**
+ * Engines is a group holding all instances of Engine
+ * This in the main is used as engines and selectedEngines to group together
+ * the engines that are selected and those that aren't
+ */
 public class Engines extends Group {
 
-	public Engines() {	//Class 'Engines' which is an extension of 'Group' 
-						//Scene2d groups store actors together so that actions
-	}					//can be performed on all actors within group
-						//The Engines Group stores together all the engines on the map
-						//We instantiate the engines group twice within the code;
-						//firstly SelectedEngines and also Engines. Engines stores all engines 
-						//that are on the map and SelectedEngines contains all Engines that are currently selected.
+	public Engines() {
+						
+	}
 	
-	public FireEngine getFireEngine(int index) {    					//Returns a fire engine actor
+	/**
+	 * Gets the FireEngine instance from the index given in the group
+	 * @param index the index in the group of the FireEngine you want returned
+	 * @return FireEngine
+	 */
+	public FireEngine getFireEngine(int index) {
 		Engine thisEngine = (Engine) this.getChild(index);
 		return thisEngine.getFireEngine();
 	}
 	
+	/**
+	 * Gets the FireEngine instance with a given ID from within the group. 
+	 * @param ID of the FireEngine you want returned
+	 * @return FireEngine if one exists with the ID given, if not returns null
+	 */
 	public FireEngine getFireEngineByID(int ID) {
 		if(this.checkInEngines(ID)) {
 			for (int i = 0; i<this.getChildren().size; i++) {
@@ -34,6 +45,11 @@ public class Engines extends Group {
 		return null;
 	}
 	
+	/**
+	 * Gets the Engine instance with the given ID from within the group
+	 * @param ID of the Engine you want returned
+	 * @return Engine if one exists with the ID given, if not returns null
+	 */
 	public Engine getEngineByID(int ID) {
 		if(this.checkInEngines(ID)) {
 			for (int i = 0; i<this.getChildren().size; i++) {
@@ -49,6 +65,11 @@ public class Engines extends Group {
 		return null;
 	}
 	
+	/**
+	 * Checks if a fireEngine with the given ID exists within the group
+	 * @param ID to check if its in the group
+	 * @return true if the ID is in the group, false if not
+	 */
 	public boolean checkInEngines(int ID) {
 		for (int i = 0; i<this.getChildren().size; i++) {
         	int var = this.getFireEngine(i).getID();
@@ -61,6 +82,9 @@ public class Engines extends Group {
 		return false;
 	}
 	
+	/**
+	 * Sets the last position of the engines
+	 */
 	public void setLastPostions() {
 		if (this.hasChildren() == true) {
 			for (Actor a : this.getChildren()) {
@@ -71,6 +95,9 @@ public class Engines extends Group {
 		}
 	}
 	
+	/**
+	 * Updates the direction of the engines in the group
+	 */
 	public void updateDir() {
 		if (this.hasChildren() == true) {
 			for (Actor a : this.getChildren()) {
@@ -85,7 +112,12 @@ public class Engines extends Group {
 
 	}
 	
-	public Engine getEngine(int index) {								//Returns a fire engine actor and it accompanying statbars
+	/**
+	 * Gives the Engine in the group at a given index
+	 * @param index
+	 * @return Engine at index
+	 */
+	public Engine getEngine(int index) {
 		return (Engine)this.getChild(index);
 	}
 
